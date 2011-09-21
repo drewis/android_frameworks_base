af50f6be18fae2ff990d18550ba59b40dec56a10
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2011-09-21 02:46:38 +0000

    minor changes
    Change-Id: Ia88e128f7664aeafab7f5691e3ec0628d7013fb0

--- core/java/com/android/internal/widget/FuzzyClock.java
+++ core/java/com/android/internal/widget/FuzzyClock.java
@@ -236,19 +236,19 @@ public class FuzzyClock extends LinearLayout {
                 timestring = timeH + " o\'clock";break;
             //five past
             case 5: case 6: case 7: case 8: case 9:
-                timestring = "five past " + timeH; break;
+                timestring = "five after " + timeH; break;
             //ten past
             case 10: case 11: case 12: case 13: case 14:
-                timestring = "ten past " + timeH; break;
+                timestring = "ten after " + timeH; break;
             //a quarter past
             case 15: case 16: case 17: case 18: case 19:
-                timestring = "a quarter past " + timeH; break;
+                timestring = "a quarter after " + timeH; break;
             //twenty past
             case 20: case 21: case 22: case 23: case 24:
-                timestring = "twenty past " + timeH; break;
+                timestring = timeH + " twenty"; break;
             //twenty-five past
             case 25: case 26: case 27: case 28: case 29:
-                timestring = "twenty five past " + timeH; break;
+                timestring = "twenty five after " + timeH; break;
             //half past
             case 30: case 31: case 32: case 33: case 34:
                 timestring = "half past " + timeH; break;
